b7404f951bf8f2caebd30696b20ce0c587e039f0
Mason Eyre <mason.deyre@gmail.com>
2019-12-12 18:38:59 -0700

    added "completed" function
    you can strike through what you've completed and also "unstrike it"

--- src/todos/List.js
+++ src/todos/List.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Todo from './Todo';
 
-const List = ({ items }) => (
+const List = ({ items, handleComplete }) => (
   <div>
     <h1>List</h1>
     <ul>
-      { items.map( item => <li key={item.id}>{item.todoValue}</li> )}
+      { items.map( item => <Todo key={item.id} {...item} handleComplete={handleComplete}/> )}
     </ul>
   </div>
 )
